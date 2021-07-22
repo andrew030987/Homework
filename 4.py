ff0f5fdc9d54752a8d937cb309ad5832d0b79d05
andrew030987 <andrew.sev87@gmail.com>
2021-07-22 14:58:18 +0300

    final commit

--- 4.py
+++ 4.py
@@ -9,9 +9,16 @@ class Car(Product):
             raise ValueError("invalid first argument!")
         try:
             self.price = float('{:.2f}'.format(price))
+            if self.price <= 0:
+                raise Exception('Invalid second argument')
         except (ValueError, TypeError):
             print("invalid second argument!")
             self.price = None
 
     def __str__(self):
         return f"Product's volume is: {self.volume}, and price is: {self.price}"
+
+
+x = Car([12,123,123], 99)
+
+print(x)
